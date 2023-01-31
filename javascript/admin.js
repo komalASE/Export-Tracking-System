a06b18a7bcfb5ea33a6f3790f17632ee9249faa7
komalASE <komal.kumari@habilelabs.io>
2023-01-31 12:50:36 +0530

    first commit

---
+++ javascript/admin.js
@@ -0,0 +1,67 @@
+let productData = document.getElementById("conatiner")
+productData = JSON.parse(window.localStorage.getItem("entriesOfProduct"));
+console.log("line->3", productData);
+
+let loggedInUservar = localStorage.getItem("loggedInUser");
+console.log(loggedInUservar);
+
+let allEntriesOfTheProduct = JSON.parse(localStorage.getItem("entriesOfProduct"));
+console.log("line->10", allEntriesOfTheProduct);
+
+
+allEntriesOfTheProduct?.map((data) => {
+    console.log("line-->12", data.status);
+    if (data.status === "Pending") {
+        console.log("line-->14", data.status);
+        document.write(`
+            <div  class="row">
+            <div class="col-4">
+                <div class="card" style="width:400px">
+                    <div class="card-body">
+                    <img class="card-img-top" src=${data.productImg}  alt="Card image" style="width:100%">
+                    <p class="card-text">Pickup Point : ${data.productPickup}</p>
+                    <p class="card-text">Destination Address : ${data.productDestination}</p>
+                   
+                    <button id="approveBtn" onclick="approveRequest(${data.id})">Approve</button>
+                    <button id="rejectBtn" onclick="rejectRequest(${data.id})">Reject</button>
+                  
+                    </div>
+                </div>
+            </div>
+            </div>
+            <br>
+            <br>`
+        );
+    }
+})
+
+function approveRequest(id) {
+    console.log("approved", id);
+    const entries = JSON.parse(localStorage.getItem('entriesOfProduct'));
+
+    entries?.map((data) => {
+        console.log("line->43", data.id);
+        if (data.id === id) { 
+          data.status = "Approved";
+          alert("status updated successfully")
+        }
+    
+    })
+
+    localStorage.setItem("entriesOfProduct", JSON.stringify(entries));
+
+}
+
+function rejectRequest(id) {
+    console.log("rejected",id);
+    const entries = JSON.parse(localStorage.getItem('entriesOfProduct'));
+
+    entries?.map((data) => {
+        if (data.id === id) { 
+          data.status = "Rejected";
+          alert("status updated successfully")
+        }
+    
+    })
+    localStorage.setItem("entriesOfProduct",JSON.stringify(entries));
+}
---
+++ javascript/user-login.js
@@ -0,0 +1,103 @@
+// const userData = [
+//     {
+//         id: 1,
+//         name: 'Ashish',
+//         email: 'ashish@gmail.com',
+//         role: 'user',
+//         password: 'Abcd@123'
+//     },
+//     {
+//         id: 2,
+//         name: 'Ajay',
+//         email: 'ajay@gmail.com',
+//         role: 'user',
+//         password: 'Abc@123'
+//     },
+//     {
+//         id: 3,
+//         name: 'komal',
+//         email: 'komal@gmail.com',
+//         role: 'admin',
+//         password: 'Abc@123'
+//     },
+//     {
+//         id: 4,
+//         name: 'manish',
+//         email: 'manish@gmail.com',
+//         role: 'user',
+//         password: 'Abc@123'
+//     }
+// ]
+
+let userData = JSON.parse(localStorage.getItem('userdata')) ;
+console.log("userData", userData);
+
+
+function formSubmission(e) {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+   
+    if (e.target.email.value === "" || e.target.email.value === null) {
+        alert("Please enter a valid email id");
+    }
+   
+    else if (e.target.password.value === "" || e.target.password.value === null) {
+        alert("Please enter a valid password");
+    }
+
+    else {
+        for (let i = 0; i < userData.length; i++) {
+            if (userData[i].email === email) {
+                if(userData[i].password === password) {
+                    localStorage.setItem("loggedInUser", email);
+                    if(userData[i].role === "Admin"){
+                        // localStorage.setItem("loggedInUser", email);
+                        console.log(userData[i].role);
+                        window.location.href = "../html/admin.html"
+                    }
+                    else{
+                        window.location.href = "../html/product.html"
+                    }
+                }
+                else {
+                    alert("please check your password")
+                }
+                break;
+            }
+            if (i === userData.length - 1) {
+                // console.log("User not found");
+                alert("User not found");
+            }
+        }
+
+        // if (userData.some((ele) => ele.email === email)) {
+        //     // alert("user found")
+        //     if (userData.some((ele) => ele.password === password)) {
+        //         console.log("PASSWORD MATCHED");
+        //         // window.location.href = "../html/product.html"
+        //         localStorage.setItem("loggedInUser", email);
+        //         if(userData.some((ele) => ele.role === "admin")){
+        //             window.location.href = "../html/admin.html"
+        //         }
+        //         else{
+        //             window.location.href = "../html/product.html"
+        //         }
+        //     }
+        //     else{
+        //         alert("please check yoour password")
+        //     }
+
+        // }
+        // else {
+        //     console.log("Email not found, please check your email");
+        // }
+    }
+
+}
+
+
+
+
+
